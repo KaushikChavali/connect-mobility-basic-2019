cd255dd97febd3e73d5fa014dab18c5650946e98
haha <haha@tum.de>
2019-11-17 19:44:01 +0000

    Improved the efficiency of getProb() and getNextState()

--- src/movement/MultiState.java
+++ src/movement/MultiState.java
@@ -33,26 +33,7 @@ public class MultiState extends MovementModel {
         this.lastWaypoint = c;
         return p;
     }
-
-    public MultiState( final Settings settings ) {
-        super( settings );
-        this.state = State.CAFE;
-    }
-
-    public MultiState( final MultiState other ) {
-        super( other );
-
-        // Pick a random state every time we replicate rather than copying!
-        // Otherwise every node would start in the same state.
-
-        waypointTable = new WaypointTable();
-        this.state = other.getState();
-    }
-
-    public MultiState(){
-        this.state = State.CAFE;
-        waypointTable = new WaypointTable();
-    }
+    
     @Override
     public Coord getInitialLocation() {
         this.lastWaypoint = this.waypointTable.getCoordFromState(State.ENTRANCE);
@@ -62,6 +43,24 @@ public class MultiState extends MovementModel {
     @Override
     public MovementModel replicate() {
         return new MultiState(this);
+    }
+
+    // Constructors
+    public MultiState( final Settings settings ) {
+        super( settings );
+        this.state = State.ENTRANCE;
+    }
+
+    public MultiState( final MultiState other ) {
+        super( other );
+
+        this.waypointTable = new WaypointTable();
+        this.state = other.getState();
+    }
+
+    public MultiState(){
+        this.waypointTable = new WaypointTable();
+        this.state = State.ENTRANCE;
     }
 
     private enum State {
@@ -79,36 +78,57 @@ public class MultiState extends MovementModel {
 
     class WaypointTable {
 
+        // TODO further improve the efficiency
+        private int morning[][] = { //follows the table in note
+            { 0, 10, 10, 70, 10, 5 },
+            { 10, 1, 9, 70, 10, 5 },
+            { 10, 5, 70, 10, 5, 5 },
+            { 1, 1, 1, 95, 2,  5 },
+            { 4, 1, 4, 7, 84, 5 },
+            { 10, 3, 10, 42, 3, 32 } };
+        private int afternoon[][] = {//follows the table in note
+            { 0, 5, 5, 39, 5, 57},
+            { 6, 1, 6, 46, 10,  66},
+            { 6, 3, 46, 6, 3, 66 },
+            { 1, 1, 1, 72, 2,  76 },
+            { 1, 1, 1, 1, 75, 9 },
+            { 14, 4, 19, 39, 19, 95 } };
+
 
         public WaypointTable(){ }
 
-        public int[][] getProb(){
-           // System.out.println(SimClock.getTime());
+        private int[] getProb(int state) {
+            // System.out.println(SimClock.getTime());
             int time = SimClock.getIntTime();
 
             System.out.println(time + " < " + SimScenario.getInstance().getEndTime() / 2);
             if (time < ( SimScenario.getInstance().getEndTime() / 2))
-                return new int[][]{ //follows the table in note
-                        { 0, 10, 10, 70, 10, 5 },
-                        { 10, 1, 9, 70, 10, 5 },
-                        { 10, 5, 70, 10, 5, 5 },
-                        { 1, 1, 1, 95, 2,  5 },
-                        { 4, 1, 4, 7, 84, 5 },
-                        { 10, 3, 10, 42, 3, 32 } };
+                return morning[state];
             else {
                 System.out.println("using afternoon time");
-                return new int[][]{ //follows the table in note
-                        { 0, 5, 5, 39, 5, 57},
-                        { 6, 1, 6, 46, 10,  66},
-                        { 6, 3, 46, 6, 3, 66 },
-                        { 1, 1, 1, 72, 2,  76 },
-                        { 1, 1, 1, 1, 75, 9 },
-                        { 14, 4, 19, 39, 19, 95 } };
+                return afternoon[state];
             }
         }
 
-        public State getNextState(State currentState){
-            ArrayList<State> temp = new ArrayList<State>();
+        public State getNextState(State currentState) {
+            // TODO further improve the efficiency z.B. precalculate total
+            int total = 0;
+            int probs[] = getProb(currentState.getNumVal());
+            
+            for (int i = 0; i < State.values().length; i++)
+                total += probs[i];
+
+            int randomNumber = new Random().nextInt(total);
+            for (int i = 0; i < State.values().length; i++) {
+                randomNumber -= probs[i];
+                if (randomNumber < 0)
+                    return State.values()[i];
+            }
+            
+            return State.values()[State.values().length-1];
+                
+
+            /*ArrayList<State> temp = new ArrayList<State>();
 //            for(int probability: this.probs[currentState.getNumVal()]){
 //                for (int i = 0;i < probability; i++){
 //                    temp.add(State.values()[probability]);
@@ -120,17 +140,18 @@ public class MultiState extends MovementModel {
                     temp.add(State.values()[i]);
                 }
             }
-            return temp.get(new Random().nextInt(temp.size()));
+            return temp.get(new Random().nextInt(temp.size()));*/
         }
 
-        public Coord getCoordFromState(State state){
-            switch (state){
+        public Coord getCoordFromState(State state) {
+            switch (state) {
+                // TODO: return random Coord inside the region of respective state
                 case CAFE: return new Coord(100,100);
                 case TOILET: return new Coord(200,200);
                 case LEISURE: return new Coord(300,200);
                 case CLASSROOM: return new Coord(300,300);
                 case LIBRARY: return new Coord(100,300);
-                default: return new Coord(0,0);
+                default: return new Coord(0,0); // ENTRANCE
             }
         }
     }
